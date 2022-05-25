c7b35b530b8221e5aa03f1b4e1ce0eeb216174ab
JuanCarlosMartinezSevilla <jcms9@gcloud.ua.es>
2022-05-25 10:42:55 +0200

    Pruebas longitud

--- CRNN/pruebas.py
+++ CRNN/pruebas.py
@@ -1,6 +1,7 @@
 import os
-from utils import krn_tokenizer
+from utils import krn_tokenizer, calculate_STFT_array_from_src
 import time
+from config import Config
 
 def token_test():
     
@@ -11,6 +12,42 @@ def token_test():
         print(krn_tokenizer(os.path.join(path, l))) 
         time.sleep(1)
 
+def parse_lst(lst_path):
+
+    X = []
+    Y = []
+    vocabulary = set()
+
+    suma_longitudes = 0
+
+    lines = open(lst_path, 'r').read().splitlines()
+    for line in lines:
+        line_aud = line + '.wav'
+        line_kern = line + '.skm'
+        audio = os.path.join(Config.path_to_audios, line_aud)
+        kern = os.path.join(Config.path_to_kern, line_kern)
+
+        spectrogram = calculate_STFT_array_from_src(audio)
+        print(f"Forma: {spectrogram.shape}")
+        suma_longitudes += spectrogram.shape[1]
+        tokens = krn_tokenizer(kern)
+
+
+        for t in tokens:
+            vocabulary.add(t)
+
+        X.append(audio) # rutas de los espectrogramas
+        Y.append(kern)  # rutas de los audios
+
+    w2i = {symbol: idx for idx, symbol in enumerate(vocabulary)}
+    i2w = {idx: symbol for idx, symbol in enumerate(vocabulary)}
+
+    print(suma_longitudes / len(lines))
+
+    print(f"{len(X)} samples loaded with {len(w2i)}-sized vocabulary")
+    return X, Y, w2i, i2w
+
+
 if __name__ == '__main__':
 
-    token_test()
+    _, _, _, _ = parse_lst('5-crossval/train_gt_fold0.dat')
